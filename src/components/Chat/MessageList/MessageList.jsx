4459fe093e98d36629b11d6f0abdedef97f5e5d7
MaSStiK <mathew.ovchinnikov@gmail.com>
2026-05-11 23:42:02 +0200

    messages list scroll to bottom

--- src/components/Chat/MessageList/MessageList.jsx
+++ src/components/Chat/MessageList/MessageList.jsx
@@ -1,48 +1,39 @@
+"use client"
+import { useEffect, useRef } from "react"
 import clsx from "clsx"
 
 import "./MessageList.css"
 
 const messages = [
-  { text: "Стандартное приветствие", fromMe: false },
-  { text: "Стандартный ответ", fromMe: true },
-  { text: "Стандартный вопрос", fromMe: false },
-  { text: "Стандартный ответ", fromMe: true },
-  { text: "Нестандартный вопрос", fromMe: false },
-  { text: "А вообще это был странный вопрос", fromMe: false },
-  { text: "Нестандартный ответ", fromMe: true },
-  { text: "Нестандартная шутка", fromMe: false },
-  { text: "Ладно, это было не смешно", fromMe: false },
-  { text: "Нестандартная реакция 😄", fromMe: true },
-  { text: "Нестандартное сообщение", fromMe: true },
-  { text: "Стандартное завершение", fromMe: false },
-  { text: "Стандартный ответ", fromMe: true },
-  { text: "Стандартное приветствие", fromMe: false },
-  { text: "Стандартный ответ", fromMe: true },
-  { text: "Стандартный вопрос", fromMe: false },
-  { text: "Стандартный ответ", fromMe: true },
-  { text: "Нестандартный вопрос", fromMe: false },
-  { text: "А вообще это был странный вопрос", fromMe: false },
-  { text: "Нестандартный ответ", fromMe: true },
-  { text: "Нестандартная шутка", fromMe: false },
-  { text: "Ладно, это было не смешно", fromMe: false },
-  { text: "Нестандартная реакция 😄", fromMe: true },
-  { text: "Нестандартное сообщение", fromMe: true },
-  { text: "Стандартное завершение", fromMe: false },
-  { text: "Стандартный ответ", fromMe: true },
+    { text: "Стандартное приветствие", fromMe: false },
+    { text: "Стандартный ответ", fromMe: true },
+    { text: "Стандартный вопрос", fromMe: false },
+    { text: "Стандартный ответ", fromMe: true },
+    { text: "Нестандартный вопрос", fromMe: false },
+    { text: "А вообще это был странный вопрос", fromMe: false },
+    { text: "Нестандартный ответ", fromMe: true },
+    { text: "Нестандартная шутка", fromMe: false },
+    { text: "Ладно, это было не смешно", fromMe: false },
+    { text: "Нестандартная реакция 😄", fromMe: true },
+    { text: "Нестандартное сообщение", fromMe: true },
+    { text: "Стандартное завершение", fromMe: false },
+    { text: "Стандартный ответ", fromMe: true },
+    { text: "Стандартное приветствие", fromMe: false },
+    { text: "Стандартный ответ", fromMe: true },
+    { text: "Стандартный вопрос", fromMe: false },
+    { text: "Стандартный ответ", fromMe: true },
+    { text: "Нестандартный вопрос", fromMe: false },
+    { text: "А вообще это был странный вопрос", fromMe: false },
+    { text: "Нестандартный ответ", fromMe: true },
+    { text: "Нестандартная шутка", fromMe: false },
+    { text: "Ладно, это было не смешно", fromMe: false },
+    { text: "Нестандартная реакция 😄", fromMe: true },
+    { text: "Нестандартное сообщение", fromMe: true },
+    { text: "Стандартное завершение", fromMe: false },
+    { text: "Стандартный ответ", fromMe: true }
 ]
 
-export default function MessageList() {
-    return (
-        <div className="flex-col gap-3 message-list">
-            {messages.map((message, i) => (
-                <Message message={message} key={i} />
-            ))}
-        </div>
-    )
-}
-
 function Message({ message }) {
-    // Добавляем стиль-модификатор перед передаваемыми классами
     const classes = clsx(
         "message",
         {
@@ -53,6 +44,31 @@ function Message({ message }) {
     return (
         <div className={classes}>
             <span>{message.text}</span>
+        </div>
+    )
+}
+
+export default function MessageList() {
+    const bottomRef = useRef(null)
+    const isFirstRender = useRef(true)
+
+    useEffect(() => {
+        bottomRef.current?.scrollIntoView({
+            behavior: isFirstRender.current
+                ? "auto"
+                : "smooth"
+        })
+
+        isFirstRender.current = false
+    }, [messages])
+
+    return (
+        <div className="flex-col gap-3 message-list">
+            {messages.map((message, i) => (
+                <Message message={message} key={i} />
+            ))}
+            {/* Якорь внизу */}
+            <div ref={bottomRef} />
         </div>
     )
 }
